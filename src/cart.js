0adbd5ae5c61b5ce6747a9664be627e9a7788a6b
Ciro Nunes <ciroanunes@gmail.com>
2015-04-23 01:05:44 -0300

    Initial commit

---
+++ src/cart.js
@@ -0,0 +1,87 @@
+import {bootstrap, Component, View, For, If} from 'angular2/angular2';
+
+class CatalogService {
+    items: List<Object>;
+
+    constructor() {
+        this.items = [
+            { id: 0, name: 'Ferrari', price: 2e5, photo: 'http://png-1.findicons.com/files/icons/1012/racing_cars/256/ferrari.png', description: 'The nicest car ever!' },
+            { id: 1, name: 'Viper', price: 1e5, photo: 'http://findicons.com/files/icons/1012/racing_cars/128/dodge.png', description: 'For you that loves performance.' }
+        ];
+    }
+}
+
+class CartService {
+    items: List<Object>;
+
+    constructor() {
+        this.items = [];
+    }
+
+    addItem(newItem) {
+        var existentItems = this.items.filter(function(item) {
+            return item.id === newItem.id
+        });
+
+        if (existentItems.length) {
+            existentItems[0].quantity += 1
+        } else {
+            newItem.quantity = 1;
+            this.items.unshift(newItem);
+        }
+    }
+
+    removeItem(targetItem) {
+        var self = this;
+
+        this.items.forEach(function(item, idx) {
+            if (targetItem.id === item.id) {
+                if (item.quantity > 1) {
+                    item.quantity -= 1;
+                } else {
+                    self.items.splice(idx, 1);
+                }
+            }
+        });
+    }
+
+    checkout() {
+      this.items = [];
+    }
+
+  calculateTotal() {
+    var total = 0;
+
+    this.items.forEach(function(item) {
+      total += item.price * item.quantity;
+    });
+
+    return total;
+  }
+}
+
+@Component({
+    selector: 'shopping-cart-app',
+    injectables: [
+        CatalogService,
+        CartService
+    ]
+})
+
+@View({
+    templateUrl: 'catalog.html',
+    directives: [For, If]
+})
+class ShoppingCmp {
+    items: List<Object>;
+
+    constructor(catalog:CatalogService, cart:CartService) {
+        this.catalog = catalog;
+        this.cart = cart;
+    }
+}
+
+export function main() {
+  bootstrap(ShoppingCmp);
+}
+
---
+++ src/material-catalog-item.js
@@ -0,0 +1,10 @@
+import {Component, View} from 'angular2/angular2';
+
+@Component({
+  selector: 'material-catalog-item'
+})
+@View({
+  template: `<material-catalog-item>x</material-catalog-item>`
+})
+
+export class MaterialCatalogItem {}
